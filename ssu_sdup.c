b2592af2f26db65777ec17828861a4bfa1f3475c
hgk9872 <hgk9872@gmail.com>
2022-04-13 05:00:29 -0700

    directory traverse implement by using queue

--- ssu_sdup.c
+++ ssu_sdup.c
@@ -13,6 +13,27 @@
 #define BUFMAX 1024
 #define ARGMAX 5
 
+// Queue를 이용한 디렉토리 탐색을 위한 노드 정의
+typedef struct Node
+{
+    char path[PATHMAX];
+    struct Node *next;
+}Node;
+
+// 디렉토리 탐색을 위한 Queue 구조체 정의
+typedef struct Queue
+{
+    Node *front;
+    Node *rear;
+    int count;
+}Queue;
+
+void InitQueue(Queue *queue);
+int isEmpty(Queue *queue);
+void Enqueue(Queue *queue, char *path);
+char *Dequeue(Queue *queue);
+
+
 int split(char *input, char *delimiter, char* argv[]);
 void command_help(void);
 void fmd5(int argc, char *argv[]);
@@ -89,8 +110,12 @@ void command_help(void)
 void fmd5(int argc, char *argv[]) 
 {
     char dirname[PATHMAX];
+    char pathname[PATHMAX];
     struct stat statbuf;
+    struct dirent** namelist;
     char *homeptr;
+    int i;
+    int count;
 
 
     // 입력 관련 에러 처리
@@ -100,8 +125,13 @@ void fmd5(int argc, char *argv[])
     }
 
     // 만약 홈디렉토리가 경로에 포함된 경우 
-    if ((homeptr = strchr(argv[4], '~')) != NULL) 
+    if ((homeptr = strchr(argv[4], '~')) != NULL) {
         sprintf(dirname, "%s%s", getenv("HOME"), homeptr + 1);
+        if(access(dirname, F_OK) < 0){
+            fprintf(stderr, "access error for %s\n", dirname);
+            return;
+        }
+    }
     else {  // 경로에 홈 디렉토리가 포함되지 않는 경우
         if (realpath(argv[4], dirname) == NULL) {
             printf("ERROR: Path exist error\n");
@@ -121,11 +151,105 @@ void fmd5(int argc, char *argv[])
         return;
     }
 
-//    printf("%s\n", getenv("HOME"));
-//    homeptr = strchr(argv[4], '~');
-//    sprintf(dirname, "%s%s", getenv("HOME"), homeptr+1); 
-    printf("AbsPath : %s\n", dirname);
+    printf("rootPath : %s\n", dirname); // 파일의 절대경로 확인
+
+    // Queue를 사용해서 BFS 디렉토리 탐색 구현
+    Queue queue;
+
+    InitQueue(&queue);
+    Enqueue(&queue, dirname); // 입력받은 경로를 root node로 시작
+
+    // Queue가 비어있을 때까지 반복
+    while (!isEmpty(&queue))
+    {
+        strcpy(dirname, Dequeue(&queue));
+        printf("Queue Path : %s\n", dirname);
+
+        // 꺼낸 큐의 디렉토리의 내부 파일목록을 불러오기
+        if((count = scandir(dirname, &namelist, NULL, alphasort)) == -1) {
+            fprintf(stderr, "scandir for %s\n", pathname);
+            exit(1);
+        }
+
+        for (i = 0; i < count; i++) {
+            if (!strcmp(namelist[i]->d_name, ".") || !strcmp(namelist[i]->d_name, ".."))
+                continue;
+
+            if (!strcmp(dirname, "/")) // 경로가 루트인 경우
+                sprintf(pathname, "%s%s", dirname, namelist[i]->d_name);
+            else
+                sprintf(pathname, "%s/%s", dirname, namelist[i]->d_name);
+
+            if ((lstat(pathname, &statbuf) < 0) && (!access(pathname, F_OK))) {
+                fprintf(stderr, "lstat error for %s\n", pathname);
+                exit(1);
+            }
+
+            if (S_ISDIR(statbuf.st_mode)) // 폴더인 경우 큐에 추가
+                Enqueue(&queue, pathname);
+//            else if (S_ISREG(statbuf.st_mode))
+//                printf("filepath : %s\n", pathname);
+
+        }
+    }
 
     return;
 
 }
+
+// Queue 초기화하는 함수
+void InitQueue(Queue *queue)
+{
+    queue->front = queue->rear = NULL; // 큐 맨앞과 맨끝 NULL 초기화
+    queue->count = 0; // 보관 개수 0으로 초기화
+}
+
+// Queue 비어있는지 확인하는 함수
+int isEmpty(Queue *queue)
+{
+    return queue->count == 0; // 보관 개수 0이면 1리턴
+}
+
+// Queue에 노드 추가하는 함수
+void Enqueue(Queue *queue, char *path)
+{
+    Node *now = (Node *)malloc(sizeof(Node));
+    strcpy(now->path, path);
+    now->next = NULL;
+
+    // 큐가 비어있을 때
+    if (isEmpty(queue))
+    {
+        queue->front = now; // 맨 앞에 노드 추가
+    }
+    else // 비어있지 않을 때
+    {
+        queue->rear->next = now; // 맨 뒤에 노드 추가
+    }
+    queue->rear = now;
+    queue->count++;
+}
+
+// 큐의 맨 앞의 원소를 뽑아 값을 리턴받음
+char *Dequeue(Queue *queue)
+{
+    static char path[PATHMAX];  // 지역변수를 리턴해야하는데, 일단 static 사용
+    Node *now;
+
+    if (isEmpty(queue)) // 큐가 비어있는 경우
+    {
+        return NULL;
+    }
+
+    now = queue->front;
+    strcpy(path, now->path);
+    queue->front = now->next;
+    free(now);
+    queue->count--;
+    return path;
+}
+    
+
+
+
+
